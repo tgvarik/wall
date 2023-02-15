31448c715cd27e400d227a3be36e106fd79c6529
Tom G Varik <tgvarik@me.com>
2023-02-15 08:52:31 -0800

    index2.js

--- index2.js
+++ index2.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const WIDTH = 108; // inches
-const HEIGHT = 54; // inches
-const PADDING = 2; // inches
-const SCALE = 8;   // pixels per inch;
+const WIDTH   = 143.50; // inches
+const HEIGHT  =  62.75; // inches
+const PADDING =   1.50; // inches
+const SCALE   =   8.00; // pixels per inch;
 
 const D3 = window['d3'];
 
@@ -16,13 +16,13 @@ const bounder = svg.append('rect')
   .style('stroke', 'black')
   .attr('class', 'bounder');
 
-const outlet = svg.append('rect')
-.attr('x', (85 + 1 / 8) * SCALE)
-.attr('y', 36 * SCALE)
-.attr('width', 2.75 * SCALE)
-.attr('height', 4.5 * SCALE)
-.style('fill', 'red')
-.style('stroke', 'black');
+// const outlet = svg.append('rect')
+// .attr('x', (85 + 1 / 8) * SCALE)
+// .attr('y', 36 * SCALE)
+// .attr('width', 2.75 * SCALE)
+// .attr('height', 4.5 * SCALE)
+// .style('fill', 'red')
+// .style('stroke', 'black');
 
 const color = D3.scale.category20();
 
@@ -41,21 +41,44 @@ const boundingRect = {
 
 const nodes = [
 //{name: 'center', width: 0, height: 0, x: width / 2, y: height / 2, fixed: true},
-  {name: 'SupremeCourt',   width: 21+ 9/16, height: 18+ 9/16}, //
-  {name: 'AkronLaw',       width: 21+ 3/ 4, height: 18+ 3/ 4},
-//{name: 'TreeOctopus',    width: 23+ 3/16, height: 35+ 5/16},
-//{name: 'Malki',          width: 14+ 1/16, height: 12+ 1/16}, //
-  {name: 'Someday',        width: 23+11/16, height: 18+ 3/ 4}, //
-  {name: 'SirIan',         width: 19+ 7/ 8, height: 25+ 1/ 8}, //
-  {name: 'Katharine',      width: 14+ 9/16, height: 17+ 9/16}, //
-//{name: 'Butterfly',      width: 10+15/16, height:  8+15/16},
-//{name: 'Ravenclaw',      width:  9+ 7/16, height:  9+ 7/16}, //
-  {name: 'DoctorHoo',      width: 29+ 3/ 8, height: 17+ 1/16}, //
-  {name: 'GoldenTreasure', width: 16+ 1/ 4, height: 13+ 3/ 4}, //
-//{name: 'Regretsy',       width: 18+ 3/16, height: 24+ 3/16},
-  {name: 'ElevenDoctors',  width: 17+ 1/ 2, height: 14+ 1/ 2}, //
-  {name: 'AynRand',        width: 15      , height: 15      }  //
+  {name: 'SupremeCourt',   width: 21+ 9/16, height: 18+ 9/16, drop: 4+ 9/16 },
+  {name: 'AkronLaw',       width: 21+11/16, height: 18+ 3/ 4, drop: 4+ 1/ 2 },
+  {name: 'Malki',          width: 14+ 1/16, height: 12+ 1/16, drop: 2+ 5/ 8 },
+  {name: 'Someday',        width: 25+23/32, height: 18+ 3/ 4, drop: 4+ 3/16 },
+  {name: 'SirIan',         width: 19+27/32, height: 25+ 3/32, drop: 7       },
+  {name: 'Katharine',      width: 14+ 9/16, height: 17+ 9/16, drop: 4+ 1/ 2 },
+  {name: 'Butterfly',      width: 10+13/16, height:  8+ 7/ 8, drop:    7/ 8 },
+  {name: 'Ravenclaw',      width:  9+ 7/16, height:  9+ 7/16, drop: 1+ 7/16 },
+  {name: 'DoctorHoo',      width: 29+ 7/16, height: 17+ 1/16, drop: 4       },
+  {name: 'GoldenTreasure', width: 16+ 3/ 4, height: 13+ 3/ 4, drop: 1+ 7/ 8 },
+  {name: 'ElevenDoctors',  width: 17+ 1/ 2, height: 14+ 1/ 2, drop: 3+ 1/ 2 },
+  {name: 'AynRand',        width: 15      , height: 15      , drop: 3+ 1/ 8 },
+  {name: 'Novy',           width: 12+ 5/ 8, height: 24      , drop: 1       },
 ];
+
+const locks = { // top-left corner!
+  SupremeCourt:   { x:  32+21/32, y:   2+21/32 },
+  AkronLaw:       { x:  60+ 3/ 8, y:  22+23/32 },
+  Malki:          { x:  55+11/16, y:   9+ 5/32 },
+  Someday:        { x:  97+11/16, y:   3+11/16 },
+  SirIan:         { x:  39+ 1/16, y:  22+23/32 },
+  Katharine:      { x:  97+21/32, y:  23+15/16 },
+  Butterfly:      { x:  71+ 7/32, y:  12+11/32 },
+  Ravenclaw:      { x: 113+11/16, y:  23+15/16 },
+  DoctorHoo:      { x:  76+29/32, y:  43       },
+  GoldenTreasure: { x:  20+13/16, y:  38+23/32 },
+  ElevenDoctors:  { x:  20+ 1/16, y:  22+23/32 },
+  AynRand:        { x:  60+13/32, y:  42+31/32 },
+  Novy:           { x:  83+17/32, y:  17+ 1/ 2 }
+};
+
+nodes.forEach(n => {
+  if (locks[n.name]) {
+    n.x = locks[n.name].x + n.width / 2;
+    n.y = locks[n.name].y + n.height / 2;
+    n.fixed = true;
+  }
+});
 
 const force = D3.layout.force()
 .size([WIDTH, HEIGHT])
@@ -99,6 +122,15 @@ const dims = svg.selectAll('.dims')
     .style('text-anchor', 'middle')
     .attr('class', 'dims')
     .text(formatLabel);
+
+const hooks = svg.selectAll('.hook')
+.data(nodes)
+.enter().append('circle')
+.attr('cx', d => d.x * SCALE)
+.attr('cy', d => ((d.y - d.height/2) + d.drop) * SCALE)
+.attr('r', 1)
+.style('fill', 'red')
+.attr('class', 'hook');
 
 force.on('tick', () => {
 
@@ -166,7 +198,7 @@ function toFraction(val) {
 }
 
 function formatLabel(n) {
-  return `${toFraction((n.x - n.width / 2) + (boundingRect.xOffset() || 0))} × ${toFraction((n.y - n.height / 2) + (boundingRect.yOffset() || 0))}`;
+  return `${toFraction(n.x + (boundingRect.xOffset() || 0))} × ${toFraction(((n.y - n.height / 2) + n.drop) + (boundingRect.yOffset() || 0))}`;
 }
 
 function testCollision(n) {
